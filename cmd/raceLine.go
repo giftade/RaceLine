39b43333663608a49728a9c4728bc9e38e561eb9
Segun Ade <giftsegunade@gmail.com>
2025-02-11 14:24:06 +0100

    feat(json): add structs to work with json data

--- cmd/raceLine.go
+++ cmd/raceLine.go
@@ -5,6 +5,48 @@ import (
 	"os"
 )
 
+type RaceInfo struct {
+	Season string `json:"season"`
+	Races []Race `json:"races"`
+	Standings Standings `json:"standings"`
+}
+
+type Race struct {
+	Round int `json:"round"`
+	RaceName string `json:"raceName"`
+	Circuit Circuit `json:"circuit"`
+	Date string `json:"date"`
+	Time string `json:"time"`
+}
+
+type Circuit struct {
+	CircuitName string `json:"circuitName"`
+	Location Location `json:"location"`
+}
+
+type Location struct {
+	Country string `json:"country"`
+	City string `json:"city"`
+}
+
+type Standings struct {
+	Drivers []Drivers `json:"drivers"`
+	Constructors []Constructors `json:"constructors"`
+}
+
+type Drivers struct {
+	Position int `json:"position"`
+	DriverName string `json:"driverName"`
+	Team string `json:"team"`
+	Points int `json:"points"`
+}
+
+type Constructors struct {
+	Position int `json:"position"`
+	Team string `json:"team"`
+	Points int `json:"points"`
+}
+
 func ListRace() error {
 	file, err := os.OpenFile("assets/mock.json", os.O_RDWR, 0644)
 	if err != nil {
@@ -18,7 +60,7 @@ func ListRace() error {
 	}
 
 	if fileStat.Size() <= 0 {
-		fmt.Println("No expenses to list")
+		fmt.Println("No race data")
 		return nil
 	}
 
