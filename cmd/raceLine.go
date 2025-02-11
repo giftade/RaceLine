cd027f40f59c2711a5e275c3e42d619e943fec5d
Segun Ade <giftsegunade@gmail.com>
2025-02-11 14:27:30 +0100

    format(structs): format text

--- cmd/raceLine.go
+++ cmd/raceLine.go
@@ -6,45 +6,45 @@ import (
 )
 
 type RaceInfo struct {
-	Season string `json:"season"`
-	Races []Race `json:"races"`
+	Season    string    `json:"season"`
+	Races     []Race    `json:"races"`
 	Standings Standings `json:"standings"`
 }
 
 type Race struct {
-	Round int `json:"round"`
-	RaceName string `json:"raceName"`
-	Circuit Circuit `json:"circuit"`
-	Date string `json:"date"`
-	Time string `json:"time"`
+	Round    int     `json:"round"`
+	RaceName string  `json:"raceName"`
+	Circuit  Circuit `json:"circuit"`
+	Date     string  `json:"date"`
+	Time     string  `json:"time"`
 }
 
 type Circuit struct {
-	CircuitName string `json:"circuitName"`
-	Location Location `json:"location"`
+	CircuitName string   `json:"circuitName"`
+	Location    Location `json:"location"`
 }
 
 type Location struct {
 	Country string `json:"country"`
-	City string `json:"city"`
+	City    string `json:"city"`
 }
 
 type Standings struct {
-	Drivers []Drivers `json:"drivers"`
+	Drivers      []Drivers      `json:"drivers"`
 	Constructors []Constructors `json:"constructors"`
 }
 
 type Drivers struct {
-	Position int `json:"position"`
+	Position   int    `json:"position"`
 	DriverName string `json:"driverName"`
-	Team string `json:"team"`
-	Points int `json:"points"`
+	Team       string `json:"team"`
+	Points     int    `json:"points"`
 }
 
 type Constructors struct {
-	Position int `json:"position"`
-	Team string `json:"team"`
-	Points int `json:"points"`
+	Position int    `json:"position"`
+	Team     string `json:"team"`
+	Points   int    `json:"points"`
 }
 
 func ListRace() error {
