6f3ce5bd119cac291332822f3a7405828b1e6402
Segun Ade <giftsegunade@gmail.com>
2025-02-12 14:56:51 +0100

    initialise file for schedule command

---
+++ cmd/schedule.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// scheduleCmd represents the schedule command
+var scheduleCmd = &cobra.Command{
+	Use:   "schedule",
+	Short: "Lists the full season race schedule",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		ListRace()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(scheduleCmd)
+
+}
